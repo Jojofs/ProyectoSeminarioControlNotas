e1750b976b611afa07ba01aa17b65f5196618825
Jonathán Florián <jojofs.uwu@gmail.com>
2024-09-28 06:02:21 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoSeminarioControlNotas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoSeminarioControlNotas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
